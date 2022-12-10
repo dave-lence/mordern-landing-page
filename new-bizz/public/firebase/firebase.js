13059924f0205e8ce9d1d20d3af4b89a78f9bc34
dave-lence <88909688+dave-lence@users.noreply.github.com>
2022-12-10 12:03:58 +0100

    Add files via upload

---
+++ new-bizz/public/firebase/firebase.js
@@ -0,0 +1,12 @@
+var  firebaseConfig = {
+    apiKey: "AIzaSyC7iSizra4LGdrcGn0_dMRLzEKv5TqJhSY",
+    authDomain: "newbizz-demo.firebaseapp.com",
+    projectId: "newbizz-demo",
+    storageBucket: "newbizz-demo.appspot.com",
+    messagingSenderId: "620393717707",
+    appId: "1:620393717707:web:11299e30c3048afcf4604e",
+  };
+
+   // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
+   const db = firebase.firestore();
